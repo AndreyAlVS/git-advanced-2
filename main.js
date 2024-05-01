66f981a34f6fe00c8cf6412279527839af30aa38
AndreyAlVS <aavasquezs@gmail.com>
2024-05-01 09:57:19 +0300

    Revert "adding result field instead of alert"
    This reverts commit 1c9f8b890cdd43c31c44647f34f1497de3c5edbd.

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     let textArea = document.querySelector('#text');
     let button = document.querySelector('#count-button');
-    let result = document.querySelector('#result');
     button.addEventListener('click', function(){
         let text = textArea.value;
-        result.innerHTML = '<b>Длина текста: </b>' +
-            text.length + ' символов';
+        alert('Длина текста: ' + text.length + ' символов');
     });
 });
