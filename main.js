eb318f60ec85fce9b683ef2bae691ae0e0ff8001
AndreyAlVS <aavasquezs@gmail.com>
2024-05-01 09:57:15 +0300

    merge hw3

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
-(function(){
-    let countSpaces = function(text) {
-        let matches = text.matchAll(/\s/g);
-        let count = 0;
-        for(let match of matches) {
-            count++;
-        }
-        return count;
-    }
-
-    document.addEventListener('DOMContentLoaded', function(){
-        let textArea = document.querySelector('#text');
-        let button = document.querySelector('#count-button');
-        let result = document.querySelector('#result');
-        button.addEventListener('click', function(){
-            let text = textArea.value;
-            let withoutSpaces = text.length - countSpaces(text);
-            result.innerHTML = '<b>Длина текста:</b> ' +
-                text.length + ' символов<br>' +
-                '<b>Без пробелов:</b> ' + withoutSpaces + '<br>';
-        });
-
-        textArea.addEventListener('keyup', function(){
-            if (textArea.value.length > 0) {
-                button.removeAttribute('disabled');
-            } else {
-                button.setAttribute('disabled', 'disabled');
-            }
-        });
+document.addEventListener('DOMContentLoaded', function(){
+    let textArea = document.querySelector('#text');
+    let button = document.querySelector('#count-button');
+    let result = document.querySelector('#result');
+    button.addEventListener('click', function(){
+        let text = textArea.value;
+        result.innerHTML = '<b>Длина текста: </b>' +
+            text.length + ' символов';
     });
-}());
+});
