a07818ebc1d7350daa9fc9ee147158f50bdac871
AndreyAlVS <aavasquezs@gmail.com>
2024-05-01 09:56:51 +0300

    merge hw3

--- main.js
+++ main.js
@@ -10,11 +10,9 @@
 
     document.addEventListener('DOMContentLoaded', function(){
         let textArea = document.querySelector('#text');
-        let countButton = document.querySelector('#count-button');
+        let button = document.querySelector('#count-button');
         let result = document.querySelector('#result');
-        let resetButton = document.querySelector('#reset-button');
-
-        countButton.addEventListener('click', function(){
+        button.addEventListener('click', function(){
             let text = textArea.value;
             let withoutSpaces = text.length - countSpaces(text);
             result.innerHTML = '<b>Длина текста:</b> ' +
@@ -24,14 +22,10 @@
 
         textArea.addEventListener('keyup', function(){
             if (textArea.value.length > 0) {
-                countButton.removeAttribute('disabled');
+                button.removeAttribute('disabled');
             } else {
-                countButton.setAttribute('disabled', 'disabled');
+                button.setAttribute('disabled', 'disabled');
             }
-        });
-
-        resetButton.addEventListener('click', function(){
-            textArea.value = '';
         });
     });
 }());
